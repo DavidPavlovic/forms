ead4e4c83178b78a1b768387f2745bd20dc7358b
David <davidpavlovicdev@gmail.com>
2022-03-25 11:06:45 +0100

    refactor simple input component

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,13 +1,11 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 
 const SimpleInput = (props) => {
-    const nameInputRef =  useRef();
     const [enteredName, setEnteredName] = useState('');
-    const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
     const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+    const enteredNameIsValid = enteredName.trim() !== '';
     const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
     const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control';
-
 
     const nameInputChangeHandler = event => {
         setEnteredName(event.target.value);
@@ -22,17 +20,10 @@ const SimpleInput = (props) => {
 
         setEnteredNameTouched(true)
 
-        if(enteredName === '') {
-            setEnteredNameIsValid(false)
-            return;
-        }
-
-        setEnteredNameIsValid(true)
-
-        const enteredValue = nameInputRef.current.value;
-        console.log(enteredValue)
+        if(!enteredNameIsValid) return;
 
         setEnteredName('');
+        setEnteredNameTouched(false);
     };
 
     
@@ -41,7 +32,6 @@ const SimpleInput = (props) => {
             <div className={nameInputClasses}>
                 <label htmlFor='name'>Your Name</label>
                 <input
-                    ref={nameInputRef}
                     type='text'
                     id='name'
                     onChange={nameInputChangeHandler}
